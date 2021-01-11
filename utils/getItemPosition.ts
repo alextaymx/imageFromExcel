81d6130189563c81875de39bc55905627d691dd6
alextaymx <alextaymx@gmail.com>
2021-01-11 17:36:37 +0800

    simplify and restructure getItemPosition code (from hafizhan)

--- utils/getItemPosition.ts
+++ utils/getItemPosition.ts
@@ -2,12 +2,13 @@ import * as XLSX from "xlsx";
 
 export const getItemPosition = (
   worksheet: XLSX.WorkSheet,
-  existingJanCode: string | any[]
+  existingJanCode: string[]
 ) => {
   const ref = worksheet["!ref"];
   const range = XLSX.utils.decode_range(ref as string);
   const itemPosition = [];
-  let codeAddress = null;
+  const targetedColName = ["barcode", "jancode"];
+  let targetedColIndex = -1;
   for (let R = range.s.r; R <= range.e.r; R++) {
     for (let C = range.s.c; C <= range.e.c; C++) {
       const cellRef = XLSX.utils.encode_cell({ c: C, r: R });
@@ -15,40 +16,52 @@ export const getItemPosition = (
       const cell = worksheet[cellRef];
       const vals = cell.v.toString();
       const checkVal = vals.replace(/[^0-9]/g, "");
-      if (existingJanCode.length > 0) {
-        let duplicate = false;
-        for (let jC of existingJanCode) {
-          if (checkVal === jC) duplicate = true;
-        }
-        if (duplicate === false) {
-          const isBarcode = checkVal.length;
-          const isLength = vals.length;
-          if (
-            isBarcode > 10 &&
-            typeof cell.v !== "object" &&
-            isLength > 10 &&
-            isLength < 15
-          )
-            itemPosition.push(cellRef);
-        }
-      } else {
-        const isBarcode = checkVal.length;
-        const isLength = vals.length;
+      if (!existingJanCode.includes(checkVal)) {
+        const trimmedLength = checkVal.length;
+        const oriLength = vals.length;
         if (
-          isBarcode > 10 &&
+          trimmedLength > 10 &&
           typeof cell.v !== "object" &&
-          isLength > 10 &&
-          isLength < 15
+          oriLength > 10 &&
+          oriLength < 15
         )
           itemPosition.push(cellRef);
       }
-      if (vals === "Barcode") {
-        codeAddress = C;
+      // if (existingJanCode.length > 0) {
+      //   // let duplicate = false;
+      //   // const duplicate = existingJanCode.includes(checkVal);
+      //   // for (let jC of existingJanCode) {
+      //   //   if (checkVal === jC) duplicate = true;
+      //   // }
+      //   if (!existingJanCode.includes(checkVal)) {
+      //     const isBarcode = checkVal.length;
+      //     const isLength = vals.length;
+      //     if (
+      //       isBarcode > 10 &&
+      //       typeof cell.v !== "object" &&
+      //       isLength > 10 &&
+      //       isLength < 15
+      //     )
+      //       itemPosition.push(cellRef);
+      //   }
+      // } else {
+      //   const isBarcode = checkVal.length;
+      //   const isLength = vals.length;
+      //   if (
+      //     isBarcode > 10 &&
+      //     typeof cell.v !== "object" &&
+      //     isLength > 10 &&
+      //     isLength < 15
+      //   )
+      //     itemPosition.push(cellRef);
+      // }
+      if (targetedColName.includes(vals.toLowerCase())) {
+        targetedColIndex = C;
       }
-      if (codeAddress !== null && codeAddress === C) {
+      if (targetedColIndex !== -1 && targetedColIndex === C) {
         if (
           vals !== undefined &&
-          vals !== "Barcode" &&
+          !targetedColName.includes(vals.toLowerCase()) &&
           checkVal.length < 10 &&
           typeof cell.v !== "object"
         ) {
